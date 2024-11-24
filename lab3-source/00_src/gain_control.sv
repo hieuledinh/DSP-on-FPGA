ccc71ac153b9c87bbeecb773e9ea9cdd7d9fb0ca
hieuledinh <info.leehieu@gmail.com>
2024-11-24 09:01:16 +0700

    add lab3 project

---
+++ lab3-source/00_src/gain_control.sv
@@ -0,0 +1,19 @@
+module gain_control(
+    input logic clk,
+    input logic [2:0] sw,
+    output logic [7:0] gain
+);
+case (sw)
+    3'b000: gain <= 8'b00000000; //0 
+    3'b001: gain <= 8'b00100000; // 0.25
+    3'b010: gain <= 8'b01000000; //0.5 
+    3'b011: gain <= 8'b01100000; //0.75
+    3'b100: gain <= 8'b10000000; //1
+    3'b101: gain <= 8'b10100000; //1.25
+    3'b110: gain <= 8'b11000000; //1.5
+    3'b111: gain <= 8'b11100000; //1.5
+    default: begin
+        gain <= 8'b10000000; //1
+    end
+endcase
+endmodule
